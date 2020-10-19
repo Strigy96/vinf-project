c5dca85c3d2d53775910279ab9f9a7b26c48ee7c
Adam Strigáč <32240034+Strigy96@users.noreply.github.com>
2020-10-19 21:59:02 +0200

    Add files via upload
    novsia verzia suboru

--- parsovanie.py
+++ parsovanie.py
@@ -13,17 +13,32 @@ if os.path.isfile('data/link.txt'):
 else:
     link = open("data/link.txt","w+",encoding='utf-8')
 
+if os.path.isfile('data/sekcia.txt'):
+    os.remove("data/sekcia.txt")
+    sekcia = open("data/sekcia.txt","w+",encoding='utf-8')
+else:
+    sekcia = open("data/sekcia.txt","w+",encoding='utf-8')
+
+pomocna = 0
 with open('data/skwiki-latest-pages-articles.xml', encoding='utf-8') as file:
     for line in file:
         if '<title>' in line:
             title = re.search('<title>(.*)</title>', line)
             title = title.group(1)
+        if pomocna == 1:
+            link.write((line + '\n'))
+        if '</text' in line:
+            pomocna = 0
         if '<text' in line:
+            link.write((line + '\n'))
+            pomocna = 1
             if '>#redirect' in line or '>#REDIRECT' in line or '>#PRESMERUJ' in line or '>#presmeruj' in line:        
                 odkaz = re.search('\[\[(.*)\]\]', line).group(1)
-                presmeruj.write((title + ' - ' + odkaz + '\n'))
-            elif '[[' in line:
-                odkaz = re.search('\[\[(.*)\]\]', line).group(1)
-                link.write((odkaz + '\n'))
+                if '#' in odkaz:
+                    #odkaz na sekciu
+                    section = re.search('#(.*)', odkaz).group(1)
+                    sekcia.write((title + ' - ' + section + '\n'))
+                else:
+                    presmeruj.write((title + ' - ' + odkaz + '\n'))
 
 presmeruj.close()        
